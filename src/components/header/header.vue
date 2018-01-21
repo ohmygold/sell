ff5c1bb9db059d0bb0fd00d4f1681c5734a7d29d
ohmygold <499452621@qq.com>
2018-01-21 18:02:13 +0800

    two

---
+++ src/components/header/header.vue
@@ -0,0 +1,23 @@
+<template>
+ <div class="header">
+11111111
+ </div>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+
+     }
+   },
+   components: {
+
+   }
+ }
+</script>
+
+<style>
+
+ 
+</style>
